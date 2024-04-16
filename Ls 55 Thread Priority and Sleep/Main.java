ce847073e98ef9597136cfc5b7f7958fc61dcf63
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-04-17 00:36:07 +0530

    Ls 55 Thread Priority and Sleep

--- Ls 55 Thread Priority and Sleep/Main.java
+++ Ls 55 Thread Priority and Sleep/Main.java
@@ -1,3 +1,49 @@
+import javax.xml.datatype.Duration;
+
+class A extends Thread{
+    public void run(){
+        for(int i=0; i<100; i++){
+            this.setPriority(Thread.MAX_PRIORITY);
+            System.out.println("Hello "+i);
+            try{
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            this.setPriority(Thread.MIN_PRIORITY);
+        }
+    }
+
+}
+
+class B extends Thread{
+    public void run(){
+        for(int i=0; i<100; i++){
+            this.setPriority(Thread.MAX_PRIORITY);
+            System.out.println("Hi "+i);
+            try{
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            this.setPriority(Thread.MIN_PRIORITY);
+        }
+    }
+}
+
 public class Main {
-    
+    public static void main(String[] args) {
+
+        A objA = new A();
+        B objB = new B();
+
+        objA.start();
+        objB.start();
+
+        System.out.println("objA Priority -> "+objA.getPriority());
+        System.out.println("objB Priority -> "+objB.getPriority());
+
+    }
 }
