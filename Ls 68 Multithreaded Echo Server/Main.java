ad5ab7a8add1b8475ac8dbdd2d69a866ad7947d1
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2025-06-13 01:46:01 +0530

    add Main.java for Ls68

---
+++ Ls 68 Multithreaded Echo Server/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    private static final int SERVER_PORT = 10000;
+    public static void main(String[] args) throws Exception {
+        try(Scanner sc = new Scanner(System.in);){
+            System.out.println("Are you a server? (y/n) : ");
+            if(sc.nextLine().equalsIgnoreCase("y")) {
+                new MyServer().start(SERVER_PORT);
+            }else{
+                new MyClient().start(SERVER_PORT, sc);
+            }
+        }
+    }
+}
